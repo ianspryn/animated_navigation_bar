17c813b0d11b4ff0926c2aac18c5314cbfafcdbc
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2019-11-13 16:27:48 -0500

    update gitignore

---
+++ animated_navigation_bar/android/app/src/debug/gen/com/spryn/modern_navigation_bar/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.spryn.modern_navigation_bar;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
